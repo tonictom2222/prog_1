5dff8550858d8156568f376393e415f6a568d602
tonictom2222 <113923566+tonictom2222@users.noreply.github.com>
2023-03-16 18:08:04 +0100

    first commit

---
+++ python/harrypotter.py
@@ -0,0 +1,9 @@
+hp = "Harry Potter"
+
+print("1. " + hp + " és a bölcsek köve")
+print("2. " + hp + " és a titkok kamrája")
+print("3. " + hp + " és az azkabani fogoly")
+print("4. " + hp + " és a tűz serlege")
+print("5. " + hp + " és a félvér herceg")
+print("6. " + hp + " és a félvér herceg")
+print("7. " + hp + " és a halál ereklyéi")
---
+++ python/szamok.py
@@ -0,0 +1,11 @@
+elso_szam= 10
+masodik_szam= 3.0
+osszeg= elso_szam + masodik_szam
+kivonas= elso_szam - masodik_szam
+szorzas= elso_szam * masodik_szam
+osztas= elso_szam / masodik_szam
+
+print(osszeg,   type(osszeg))
+print(kivonas,  type(osszeg))
+print(szorzas,  type(osszeg))
+print(osztas,   type(osszeg))

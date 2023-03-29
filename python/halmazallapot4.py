5e01eee7f6553e308b1b32adbe763f0abc4914fb
tonictom2222 <113923566+tonictom2222@users.noreply.github.com>
2023-03-29 20:42:53 +0200

    new code

--- python/halmazallapot4.py
+++ python/halmazallapot4.py
@@ -1,1 +1,9 @@
-input(int('cock'))
+while True:
+    viz= float(input('Víz hőmérséklete: '))
+
+    if viz>= 100:
+        print('légnemű')
+    elif viz< 0:
+        print('szilárd')
+    else:
+        print('folyékony')
